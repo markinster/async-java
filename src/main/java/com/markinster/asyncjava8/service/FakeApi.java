1411d8aa69e7d3a492b4b3f2add6e60de6d84fcf
markinster <markinster@gmail.com>
2016-09-13 13:49:10 -0300

    implemmented sleep

--- src/main/java/com/markinster/asyncjava8/service/FakeApi.java
+++ src/main/java/com/markinster/asyncjava8/service/FakeApi.java
@@ -3,13 +3,15 @@ package com.markinster.asyncjava8.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.jmx.snmp.Timestamp;
+
 public class FakeApi {
-	
-	private int temp1 = 500;
-	private int temp2 = 200;
 	
 	public List<String> service1() {
 		System.out.println("[ service 1 ] Started");	
+		
+		sleep(1500);		
+		
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("São Paulo");
 		list.add("Rio de Janeiro");
@@ -20,17 +22,15 @@ public class FakeApi {
 		list.add("Fortaleza");
 		list.add("Maceio");
 		
-		String x = "";
-		for(int i = 0; i < temp1; i++)
-			for(int j = 0; j < temp2; j++)
-					x = x + ".";
-
 		System.out.println("[ service 1 ] Finished");
 		return list;
 	}
 	
 	public List<String> service2() {
-		System.out.println("[ service 2 ] Started");	
+		System.out.println("[ service 2 ] Started");
+		
+		sleep(1500);
+		
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("Minas Gerais");
 		list.add("Rio de Janeiro");
@@ -39,33 +39,38 @@ public class FakeApi {
 		list.add("Fortaleza");
 		list.add("Santa Catarina");
 		list.add("Alagoas");
-		list.add("Ceará");
+		list.add("Ceará");		
 		
-		String x = "";
-		for(int i = 0; i < temp1; i++)
-			for(int j = 0; j < temp2; j++)
-					x = x + ".";
 
 		System.out.println("[ service 2 ] Finished");
 		return list;
 	}
 	
 	public List<String> service3() {
-		System.out.println("[ service 3 ] Started");	
+		System.out.println("[ service 3 ] Started");
+		
+		sleep(2000);
+		
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("Sudeste");
 		list.add("Centro-Oeste");
 		list.add("Nordeste");
 		list.add("Norte");
-		list.add("Sul");
-		
-		String x = "";
-		for(int i = 0; i < temp1; i++)
-			for(int j = 0; j < temp2; j++)
-					x = x + ".";
+		list.add("Sul");	
+	
 		
 		System.out.println("[ service 3 ] Finished");	
 		return list;
+	}
+	
+	private void sleep(long time) {
+		long ti = new Timestamp().getDateTime();
+		long sl = 0l;
+		
+		while (sl  < time) {
+			long tf = new Timestamp().getDateTime();
+			sl = tf - ti;
+		}
 	}
 
 }
